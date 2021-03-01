ec6da898628dceb1b4f8f10b7b7edfae8baf139f
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-02 01:23:53 +0530

    Added:wages for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,20 +1,28 @@
 #!/bin/bash -x
 
 EMP_RATE_PER_HR=20
-FULL_TIME=10
-PART_TIME=8
+WORKING_DAYS=20
+FULL_TIME=1
+PART_TIME=2
+total_Salary=0
 
-empCheck=$((RANDOM%2))
-isPresent=1
-
-
+for((D=1;D<=WORKING_DAYS;D++))
+do
+empCheck=$((RANDOM%3))
 case $empCheck in 
-	1)
-	salary=$(($EMP_RATE_PER_HR*$FULL_TIME))
-	echo $salary
-	;;
-	0)
-	salary=$(($EMP_RATE_PER_HR*$PART_TIME))
-	echo $salary
-	;;
+	$FULL_TIME)
+		empHrs=12
+		;;
+	$PART_TIME)
+		empHrs=8
+		;;
+		*)
+		empHrs=0
+		;;
 esac
+salary=$(( $empHrs*$EMP_RATE_PER_HR ))
+total_Salary=$(($total_Salary+$salary))
+done
+
+echo "Salary per day:" $salary
+echo "Salary per month that is 20 working days:" $total_Salary
