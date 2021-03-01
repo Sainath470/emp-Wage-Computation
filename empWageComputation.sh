0b6d2c3316cdac01f77bb8cbaa56913e0dc8f2a3
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-02 00:42:05 +0530

    Added:part time hours 8

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 EMP_RATE_PER_HR=20
-FULL_TIME=8
-PART_TIME=4
+FULL_TIME=10
+PART_TIME=8
 
 empCheck=$((RANDOM%2))
 isPresent=1
@@ -13,6 +13,6 @@ then
 	salary=$(($EMP_RATE_PER_HR*$FULL_TIME))
 	echo $salary
 else
-	salary=0
+	salary=$(($EMP_RATE_PER_HR*$PART_TIME))
 	echo $salary
 fi
