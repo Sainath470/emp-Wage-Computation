e89a99f1a20bb59ebeb284472b86ed5df475d0c5
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-02 02:02:10 +0530

    Added:totalemphrs using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,14 +5,15 @@ WORKING_DAYS=20
 FULL_TIME=1
 PART_TIME=2
 MAX_HRS_IN_MONTH=100
+
 totalWorkingHrs=0
 totalWorkingDays=0
 total_Salary=0
-while [[ $totalWorkingHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $WORKING_DAYS ]]
-do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3))
-case $empCheck in 
+
+
+function work_Hours()
+{
+case $1 in 
 	$FULL_TIME)
 		empHrs=12
 		;;
@@ -23,9 +24,22 @@ case $empCheck in
 		empHrs=0
 		;;
 esac
-totalWorkingHrs=$(( $totalWorkingHrs+$empHrs ))
+echo $empHrs
+}
+
+
+
+while [[ $totalWorkingHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $WORKING_DAYS ]]
+do
+		((totalWorkingDays++))
+		empCheck=$((RANDOM%3))
+		empHrs=$( work_Hours $empCheck )
+		totalWorkingHrs=$(( $totalWorkingHrs+$empHrs ))
 done
+
 total_Salary=$(($totalWorkingHrs*$EMP_RATE_PER_HR))
+
+
 
 echo "total working hours:" $totalWorkingHrs
 echo "Total salary for 20 days :" $total_Salary
